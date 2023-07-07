b34c896eb1b7d60032d70ffbb652343ee4188015
snsql365 <138164427+snsql365@users.noreply.github.com>
2023-07-07 16:43:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,9 +44,7 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 # Allow the user to add a fruit to the list
-with st.form("add_fruit"):
-    add_my_fruit = st.text_area('add_my_fruit')
-    sub_comment = st.form_submit_button('Submit')
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 
 # my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 streamlit.write('Thanks for adding ', add_my_fruit)
