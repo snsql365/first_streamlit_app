2118eb2f1f9fc89e8085f5778400961da6fb9754
snsql365 <138164427+snsql365@users.noreply.github.com>
2023-07-12 10:34:31 -0500

    Update streamlit_app.py
    Alt add new fruit from:
    https://github.com/lorenzo92160/snowflake-badge-2/blob/main/streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,8 +42,21 @@ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
-
+'''
 # Allow the user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 my_cur.execute("insert into pc_rivery_db.public.fruit_load_list (fruit_name) values {add_my_fruit} ")
 streamlit.write('Thanks for adding ', add_my_fruit)
+'''
+# Allow the user to add fruits on the list
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("INSERT INTO fruit_load_list values('" + new_fruit + "')")
+    return ("Thanks for adding " + new_fruit)
+  
+add_my_fruit = streamlit.text_input('Add a fruit to the list')
+if streamlit.button("Add a fruit to the list"):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  insert_fruit_response = insert_row_snowflake(add_my_fruit)
+  my_cnx.close()
+  streamlit.text(insert_fruit_response)
